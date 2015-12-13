152231087f70afe9f645a880251978aad87deeff
bruno0022 <seliath@hotmail.es>
2015-12-13 12:36:09 +0100

    subo productor y productorDAO

---
+++ app/src/main/java/com/example/vesprada/controlpelicula/Productor.java
@@ -0,0 +1,14 @@
+package com.example.vesprada.controlpelicula;
+
+/**
+ * Created by Pc on 13/12/2015.
+ */
+public class Productor {
+    public static final String TABLE = "Productor";
+
+    public static final String KEY_ID = "ID";
+    public static final String KEY_nombre = "nombre";
+
+    public int ID;
+    public String nombre;
+}
