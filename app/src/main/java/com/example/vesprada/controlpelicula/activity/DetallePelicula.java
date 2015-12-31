7fcea7afa8822400d3e1b45cb2111364f93f034b
javicg23 <javicg23@yahoo.com>
2015-12-31 14:33:37 +0100

    Anyadido findviewbyid en DetallePelicula

--- app/src/main/java/com/example/vesprada/controlpelicula/activity/DetallePelicula.java
+++ app/src/main/java/com/example/vesprada/controlpelicula/activity/DetallePelicula.java
@@ -2,11 +2,23 @@ package com.example.vesprada.controlpelicula.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.vesprada.controlpelicula.R;
 import com.example.vesprada.controlpelicula.modelo.Pelicula;
 
 public class DetallePelicula extends AppCompatActivity {
+    private TextView tvTituloDetalle;
+    private ImageView ivPortadaDetalle;
+    private TextView tvPuntuacionDetalle;
+    private TextView tvAnyoDetalle;
+    private TextView tvDuracionDetalle;
+    private TextView tvGeneroDetalle;
+    private TextView tvDirectorDetalle;
+    private TextView tvProductorDetalle;
+    private TextView tvActoresDetalle;
+    private TextView tvSinopsisDetalle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,15 @@ public class DetallePelicula extends AppCompatActivity {
         if (extras != null) {
             Pelicula value = extras.getClass(pelicula);
         }*/
+        tvTituloDetalle = (TextView)findViewById(R.id.tvTituloDetalle);
+        ivPortadaDetalle = (ImageView)findViewById(R.id.ivPortadaDetalle);
+        tvPuntuacionDetalle = (TextView)findViewById(R.id.tvPuntuacionDetalle);
+        tvAnyoDetalle = (TextView)findViewById(R.id.tvAnyoDetalle);
+        tvDuracionDetalle = (TextView)findViewById(R.id.tvDuracionDetalle);
+        tvGeneroDetalle = (TextView)findViewById(R.id.tvGeneroDetalle);
+        tvDirectorDetalle = (TextView)findViewById(R.id.tvDirectorDetalle);
+        tvProductorDetalle = (TextView)findViewById(R.id.tvProductorDetalle);
+        tvActoresDetalle = (TextView)findViewById(R.id.tvActoresDetalle);
+        tvSinopsisDetalle = (TextView)findViewById(R.id.tvSinopsisDetalle);
     }
 }
